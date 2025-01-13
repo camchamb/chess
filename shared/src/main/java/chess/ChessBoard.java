06918f7e9d8876550f1b4be64a2de7fe5d4123fe
camchamb <cameronchamberlain1@gmail.com>
2025-01-13 14:09:02 -0700

    Fully implemented ChessBoard.resetBoard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -40,5 +43,55 @@ public class ChessBoard {
      */
     public void resetBoard() {
         board = new ChessPiece[8][8];
+        for (int x = 1; x < 9; x++) {
+            var whitePiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+            var blackPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+            addPiece(new ChessPosition(2, x), whitePiece);
+            addPiece(new ChessPosition(7, x), blackPiece);
+        }
+        int y = 1;
+        for (ChessGame.TeamColor color : ChessGame.TeamColor.values()) {
+            var RookPiece = new ChessPiece(color, ChessPiece.PieceType.ROOK);
+            var KnightPiece = new ChessPiece(color, ChessPiece.PieceType.KNIGHT);
+            var BishopPiece = new ChessPiece(color, ChessPiece.PieceType.BISHOP);
+            var KingPiece = new ChessPiece(color, ChessPiece.PieceType.KING);
+            var QueenPiece = new ChessPiece(color, ChessPiece.PieceType.QUEEN);
+            var Rook2Piece = new ChessPiece(color, ChessPiece.PieceType.ROOK);
+            var Knight2Piece = new ChessPiece(color, ChessPiece.PieceType.KNIGHT);
+            var Bishop2Piece = new ChessPiece(color, ChessPiece.PieceType.BISHOP);
+            addPiece(new ChessPosition(y, 1), RookPiece);
+            addPiece(new ChessPosition(y, 2), KnightPiece);
+            addPiece(new ChessPosition(y, 3), BishopPiece);
+            addPiece(new ChessPosition(y, 5), KingPiece);
+            addPiece(new ChessPosition(y, 4), QueenPiece);
+            addPiece(new ChessPosition(y, 8), Rook2Piece);
+            addPiece(new ChessPosition(y, 7), Knight2Piece);
+            addPiece(new ChessPosition(y, 6), Bishop2Piece);
+            y += 7;
+        }
+    }
+
+    @Override
+    public String toString() {
+        var builder = new StringBuilder();
+        for (var item : board) {
+            builder.append(Arrays.toString(item));
+            builder.append(", ");
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
     }
 }
