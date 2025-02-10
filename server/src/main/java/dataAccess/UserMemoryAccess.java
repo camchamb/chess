0cb11bc3bbf6e3ae49e42a75d8e902ac4c5e4c33
camchamb <cameronchamberlain1@gmail.com>
2025-02-10 15:54:30 -0700

    Implemented register in UserService

---
+++ server/src/main/java/dataAccess/UserMemoryAccess.java
@@ -0,0 +1,20 @@
+package dataAccess;
+
+import model.UserData;
+
+public class UserMemoryAccess implements UserDAO{
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+
+    @Override
+    public void createUser(UserData u) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+}
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,52 @@
 package service;
 
+import dataAccess.AuthDAO;
+import dataAccess.DataAccessException;
+import dataAccess.UserDAO;
+import dataAccess.UserMemoryAccess;
+import model.AuthData;
+import model.UserData;
+import java.util.UUID;
+
+record RegisterRequest (String username, String password, String email) {}
+
+record RegisterResult (String username, String authToken, String message) {}
+
+record LoginRequest (String username, String password) {}
+
+record LoginResult (String username, String authToken, String message) {}
+
+record LogoutRequest (String authToken) {}
+
+record LogoutResult (String message) {}
+
 public class UserService {
+
+    private final UserDAO userAccess;
+    private final AuthDAO authAccess;
+
+    public UserService(UserDAO userAccess, AuthDAO authAccess) {
+        this.userAccess = userAccess;
+        this.authAccess = authAccess;
+    }
+
+    private static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
+
+    public RegisterResult register(RegisterRequest registerRequest) throws DataAccessException {
+        UserData userData = userAccess.getUser(registerRequest.username());
+        if (userData != null) {
+            return new RegisterResult(null, null, "Username Taken");
+        }
+        userData = new UserData(registerRequest.username(), registerRequest.password(), registerRequest.email());
+        userAccess.createUser(userData);
+        String authToken = generateToken();
+        AuthData authData = new AuthData(authToken, registerRequest.username());
+        authAccess.createAuth(authData);
+        return new RegisterResult(registerRequest.username(), authToken, "Success");
+    }
+
+    public LoginResult login(LoginRequest loginRequest) {}
+    public void logout(LogoutRequest logoutRequest) {}
 }
