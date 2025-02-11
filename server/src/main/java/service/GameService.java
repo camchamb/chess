33ec9f9edeb9f886670520e1919effcbdb8f3efc
camchamb <cameronchamberlain1@gmail.com>
2025-02-11 15:39:35 -0700

    Added clear to GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,17 @@
 package service;
 
+import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
+
+
 public class GameService {
+    private final GameDAO gameAccess;
+
+    public GameService(GameDAO gameAccess) {
+        this.gameAccess = gameAccess;
+    }
+
+    public void clear() throws DataAccessException {
+        gameAccess.clear();
+    }
 }
