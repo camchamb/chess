6aaa3efafce8e940e56d68063fb62ca43942e9a6
camchamb <cameronchamberlain1@gmail.com>
2025-02-12 11:07:53 -0700

    Added listGames to GameService

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,17 +1,35 @@
 package service;
 
+import dataAccess.AuthDAO;
 import dataAccess.DataAccessException;
 import dataAccess.GameDAO;
+import service.Requests.ListGamesRequest;
+import service.Requests.ListGamesResult;
+import service.Requests.LogoutRequest;
 
 
 public class GameService {
     private final GameDAO gameAccess;
+    private final AuthDAO authAccess;
 
-    public GameService(GameDAO gameAccess) {
+    public GameService(GameDAO gameAccess, AuthDAO authAccess) {
         this.gameAccess = gameAccess;
+        this.authAccess = authAccess;
     }
 
     public void clear() throws DataAccessException {
         gameAccess.clear();
+    }
+
+    public ListGamesResult listGames(ListGamesRequest listGamesRequest) throws DataAccessException{
+        if (listGamesRequest.authToken() == null) {
+            throw new DataAccessException(400, "Error: invalid request");
+        }
+        var authData = authAccess.getAuth(listGamesRequest.authToken());
+        if (authData == null) {
+            throw new DataAccessException(401, "Error: unauthorized");
+        }
+        var data = gameAccess.listGames();
+        return new ListGamesResult(data);
     }
 }
