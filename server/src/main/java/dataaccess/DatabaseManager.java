9e2d3f60ce92281faee4cdaf378200afca532d3a
camchamb <cameronchamberlain1@gmail.com>
2025-03-04 10:58:17 -0700

    Changed DatabaseManager to throw correct error message

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -44,7 +44,7 @@ public class DatabaseManager {
                 preparedStatement.executeUpdate();
             }
         } catch (SQLException e) {
-            throw new DataAccessException(e.getMessage());
+            throw new DataAccessException(400, e.getMessage());
         }
     }
 
@@ -66,7 +66,7 @@ public class DatabaseManager {
             conn.setCatalog(DATABASE_NAME);
             return conn;
         } catch (SQLException e) {
-            throw new DataAccessException(e.getMessage());
+            throw new DataAccessException(400, e.getMessage());
         }
     }
 }
