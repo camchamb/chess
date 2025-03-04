f64df140c6241809637021ff9e72b5d65715f8b5
camchamb <cameronchamberlain1@gmail.com>
2025-03-04 11:00:29 -0700

    Added UserSqlAccess

---
+++ server/src/main/java/dataaccess/UserSqlAccess.java
@@ -0,0 +1,81 @@
+package dataaccess;
+
+import model.UserData;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.sql.*;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+
+
+public class UserSqlAccess implements UserDAO{
+    public UserSqlAccess() throws DataAccessException {
+        configureDatabase();
+    }
+
+
+    @Override
+    public void clear() throws DataAccessException {
+        var statement = "DELETE FROM user";
+        executeUpdate(statement);
+    }
+
+    @Override
+    public void createUser(UserData u) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+
+            var createUserTable = """
+            CREATE TABLE  IF NOT EXISTS user (
+                username VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                PRIMARY KEY (username)
+            )""";
+
+            try (var preparedStatement = conn.prepareStatement(createUserTable)) {
+                preparedStatement.executeUpdate();
+            }
+        }
+        catch (SQLException ex) {
+            throw new DataAccessException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+
+    }
+}
