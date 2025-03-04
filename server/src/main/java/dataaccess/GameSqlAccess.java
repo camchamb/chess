af59e8c2d0399f531a107d6be8896b3407655f4f
camchamb <cameronchamberlain1@gmail.com>
2025-03-04 13:39:22 -0700

    Added configureDatabase to GameSqlAccess

--- server/src/main/java/dataaccess/GameSqlAccess.java
+++ server/src/main/java/dataaccess/GameSqlAccess.java
@@ -2,10 +2,15 @@ package dataaccess;
 
 import model.GameData;
 
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
 public class GameSqlAccess implements GameDAO{
+    public GameSqlAccess() throws DataAccessException {
+        configureDatabase();
+    }
+
     @Override
     public int createGame(String gameName) throws DataAccessException {
         return 0;
@@ -28,6 +33,32 @@ public class GameSqlAccess implements GameDAO{
 
     @Override
     public void clear() throws DataAccessException {
+
+    }
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+
+            var createUserTable = """
+            CREATE TABLE  IF NOT EXISTS game (
+                gameID INT NOT NULL AUTO_INCREMENT,
+                whiteUsername VARCHAR(255) NOT NULL,
+                blackUsername VARCHAR(255) NOT NULL,
+                gameName VARCHAR(255) NOT NULL,
+                game VARCHAR(255) NOT NULL,
+                PRIMARY KEY (gameID),
+                FOREIGN KEY (whiteUsername) REFERENCES user(username) ON DELETE CASCADE,
+                FOREIGN KEY (blackUsername) REFERENCES user(username) ON DELETE CASCADE
+            )""";
+
+            try (var preparedStatement = conn.prepareStatement(createUserTable)) {
+                preparedStatement.executeUpdate();
+            }
+        }
+        catch (SQLException ex) {
+            throw new DataAccessException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
 
     }
 }
