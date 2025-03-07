0ef62f25ffc0b1eec6a99b4c83021fb4e687c435
camchamb <cameronchamberlain1@gmail.com>
2025-03-07 12:41:57 -0700

    Changed UserSqlAccess to use SqlUtils

--- server/src/main/java/dataaccess/UserSqlAccess.java
+++ server/src/main/java/dataaccess/UserSqlAccess.java
@@ -1,10 +1,7 @@
 package dataaccess;
 
 import model.UserData;
-import com.google.gson.Gson;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.sql.*;
 
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
@@ -21,13 +18,13 @@ public class UserSqlAccess implements UserDAO{
     @Override
     public void clear() throws DataAccessException {
         var statement = "DELETE FROM user";
-        executeUpdate(statement);
+        SqlUtils.executeUpdate(statement);
     }
 
     @Override
     public void createUser(UserData u) throws DataAccessException {
         var statement = "INSERT INTO user (username, password, email) VALUES(?, ?, ?)";
-        executeUpdate(statement, u.username(), u.password(), u.email());
+        SqlUtils.executeUpdate(statement, u.username(), u.password(), u.email());
     }
 
     @Override
@@ -51,33 +48,7 @@ public class UserSqlAccess implements UserDAO{
         return null;
     }
 
-    private int executeUpdate(String statement, Object... params) throws DataAccessException {
-    try (var conn = DatabaseManager.getConnection()) {
-        try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-            for (var i = 0; i < params.length; i++) {
-                var param = params[i];
-                if (param instanceof String p) ps.setString(i + 1, p);
-                else if (param instanceof Integer p) ps.setInt(i + 1, p);
-                else if (param == null) ps.setNull(i + 1, NULL);
-            }
-            ps.executeUpdate();
-
-            var rs = ps.getGeneratedKeys();
-            if (rs.next()) {
-                return rs.getInt(1);
-            }
-
-            return 0;
-        }
-    } catch (SQLException e) {
-        throw new DataAccessException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
-    }
-    }
-
     private void configureDatabase() throws DataAccessException {
-        DatabaseManager.createDatabase();
-        try (var conn = DatabaseManager.getConnection()) {
-
             var createUserTable = """
             CREATE TABLE  IF NOT EXISTS user (
                 username VARCHAR(255) NOT NULL,
@@ -85,14 +56,6 @@ public class UserSqlAccess implements UserDAO{
                 email VARCHAR(255) NOT NULL,
                 PRIMARY KEY (username)
             )""";
-
-            try (var preparedStatement = conn.prepareStatement(createUserTable)) {
-                preparedStatement.executeUpdate();
-            }
-        }
-        catch (SQLException ex) {
-            throw new DataAccessException(500, String.format("Unable to configure database: %s", ex.getMessage()));
-        }
-
+            SqlUtils.configureDatabase(createUserTable);
     }
 }
