20b5bba54e860adbc42a635a3854a56e7e83e8ac
camchamb <cameronchamberlain1@gmail.com>
2025-03-12 14:48:21 -0600

    Added eval

--- client/src/main/java/client/PostloginClient.java
+++ client/src/main/java/client/PostloginClient.java
@@ -1,4 +1,48 @@
 package client;
 
+import com.sun.nio.sctp.NotificationHandler;
+import dataaccess.DataAccessException;
+import server.ServerFacade;
+
+import java.util.Arrays;
+
 public class PostloginClient {
+    private String visitorName = null;
+    private final ServerFacade server;
+    private final String serverUrl;
+//    private State state = State.SIGNEDOUT;
+
+    public PostloginClient(String serverUrl, NotificationHandler notificationHandler) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+//        this.notificationHandler = notificationHandler;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> new DataAccessException(400, "5");
+//                case "rescue" -> rescuePet(params);
+//                case "list" -> listPets();
+//                case "signout" -> signOut();
+//                case "adopt" -> adoptPet(params);
+//                case "adoptall" -> adoptAllPets();
+//                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (DataAccessException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String help() {
+        return """
+                register <username> <password> <email> - to register
+                login <username> <password> - to login
+                quit - stop client
+                help - all commands
+                """;}
 }
