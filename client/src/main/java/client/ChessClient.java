8ff2dc97415c7c7da9437ca0dc915d44217a1b0e
camchamb <cameronchamberlain1@gmail.com>
2025-03-12 12:38:53 -0600

    Added ChessClient

---
+++ client/src/main/java/client/ChessClient.java
@@ -0,0 +1,54 @@
+package client;
+
+import com.sun.nio.sctp.NotificationHandler;
+import model.UserData;
+import server.ServerFacade;
+import dataaccess.DataAccessException;
+
+import java.util.Arrays;
+
+public class ChessClient {
+    private String visitorName = null;
+    private final ServerFacade server;
+    private final String serverUrl;
+//    private State state = State.SIGNEDOUT;
+
+    public ChessClient(String serverUrl, NotificationHandler notificationHandler) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+//        this.notificationHandler = notificationHandler;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register(params);
+//                case "rescue" -> rescuePet(params);
+//                case "list" -> listPets();
+//                case "signout" -> signOut();
+//                case "adopt" -> adoptPet(params);
+//                case "adoptall" -> adoptAllPets();
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (DataAccessException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String help() {return "help stuff";}
+
+    public String register(String... params) throws DataAccessException {
+        if (params.length < 3) {
+            throw new DataAccessException(400, "Expected: <username> <password> <email>");
+        }
+        var username = params[0];
+        var password = params[1];
+        var email = params[2];
+        var user = server.addUser(new UserData(username, password, email));
+        return "Registered:" + user.username();
+    }
+}
