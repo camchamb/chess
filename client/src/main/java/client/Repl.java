6669bd35bf8be4b4eb1b18ca2d7828719cf15454
camchamb <cameronchamberlain1@gmail.com>
2025-03-12 12:38:36 -0600

    Added Repl

---
+++ client/src/main/java/client/Repl.java
@@ -0,0 +1,42 @@
+package client;
+
+import com.sun.nio.sctp.HandlerResult;
+import com.sun.nio.sctp.Notification;
+import com.sun.nio.sctp.NotificationHandler;
+
+import java.util.Scanner;
+
+import static java.awt.Color.BLUE;
+
+public class Repl implements NotificationHandler {
+    private final ChessClient client;
+
+    public Repl(String serverUrl) {
+        client = new ChessClient(serverUrl, this);
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+            try {
+                result = client.eval(line);
+                System.out.print(BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+    }
+
+    public void printPrompt() {
+        System.out.print("\n" + "ChessClient >>> ");
+    }
+
+    @Override
+    public HandlerResult handleNotification(Notification notification, Object attachment) {
+        return null;
+    }
+}
