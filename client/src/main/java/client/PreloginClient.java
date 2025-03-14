3c5bcccfbfd8b16b26d7b8a74af348af55fe163b
camchamb <cameronchamberlain1@gmail.com>
2025-03-14 14:50:03 -0600

    Changed eval to call three different evals based on the state

--- client/src/main/java/client/PreloginClient.java
+++ client/src/main/java/client/PreloginClient.java
@@ -8,15 +8,16 @@ import dataaccess.DataAccessException;
 import java.util.Arrays;
 
 public class PreloginClient {
+    private final NotificationHandler notificationHandler;
     private String visitorName = null;
     private final ServerFacade server;
     private final String serverUrl;
-//    private State state = State.SIGNEDOUT;
+    private State state = State.PreloginClient;
 
     public PreloginClient(String serverUrl, NotificationHandler notificationHandler) {
         server = new ServerFacade(serverUrl);
         this.serverUrl = serverUrl;
-//        this.notificationHandler = notificationHandler;
+        this.notificationHandler = notificationHandler;
     }
 
     public String eval(String input) {
@@ -24,25 +25,77 @@ public class PreloginClient {
             var tokens = input.toLowerCase().split(" ");
             var cmd = (tokens.length > 0) ? tokens[0] : "help";
             var params = Arrays.copyOfRange(tokens, 1, tokens.length);
-            return switch (cmd) {
-                case "register" -> register(params);
-//                case "rescue" -> rescuePet(params);
-//                case "list" -> listPets();
-//                case "signout" -> signOut();
-//                case "adopt" -> adoptPet(params);
-//                case "adoptall" -> adoptAllPets();
-                case "quit" -> "quit";
-                default -> help();
+            return switch (state) {
+                case PreloginClient -> preEval(params, cmd);
+                case PostloginClient -> postEval(params, cmd);
+                case GamePlayClient -> gameEval(params, cmd);
             };
         } catch (DataAccessException ex) {
             return ex.getMessage();
         }
     }
 
-    public String help() {
+    public String preEval(String[] params, String cmd) throws DataAccessException{
+        return switch (cmd) {
+            case "register" -> register(params);
+//                case "rescue" -> rescuePet(params);
+//                case "list" -> listPets();
+//                case "signout" -> signOut();
+//                case "adopt" -> adoptPet(params);
+//                case "adoptall" -> adoptAllPets();
+            case "quit" -> "quit";
+            default -> preHelp();
+        };
+    }
+
+    public String postEval(String[] params, String cmd) throws DataAccessException{
+        return switch (cmd) {
+            case "register" -> register(params);
+//                case "rescue" -> rescuePet(params);
+//                case "list" -> listPets();
+//                case "signout" -> signOut();
+//                case "adopt" -> adoptPet(params);
+//                case "adoptall" -> adoptAllPets();
+            case "quit" -> "quit";
+            default -> preHelp();
+        };
+    }
+
+    public String gameEval(String[] params, String cmd) throws DataAccessException{
+        return switch (cmd) {
+            case "register" -> register(params);
+//                case "rescue" -> rescuePet(params);
+//                case "list" -> listPets();
+//                case "signout" -> signOut();
+//                case "adopt" -> adoptPet(params);
+//                case "adoptall" -> adoptAllPets();
+            case "quit" -> "quit";
+            default -> preHelp();
+        };
+    }
+
+    public String preHelp() {
         return """
                 register <username> <password> <email> - to register
                 login <username> <password> - to login
+                quit - stop client
+                help - all commands
+                """;}
+
+    public String postHelp() {
+        return """
+                create <GameName> - to create a game
+                list - list all games
+                join <ID> <WHITE|BLACK> - Join game as a color
+                observe <ID> - observe game
+                logout - logout of server
+                quit - stop client
+                help - all commands
+                """;}
+
+    public String gameHelp() {
+        return """
+                future work
                 quit - stop client
                 help - all commands
                 """;}
