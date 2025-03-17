d7badd50c0f08f72852c52ea4d2e7a7babae19c3
camchamb <cameronchamberlain1@gmail.com>
2025-03-17 11:38:10 -0600

    Removed seperate chess clients. Changed it to one client class.

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -7,30 +7,22 @@ import com.sun.nio.sctp.NotificationHandler;
 import java.util.Scanner;
 
 public class Repl implements NotificationHandler {
-    private final PreloginClient preClient;
-    private final PostloginClient postClient;
-    private final GamePlayClient gameClient;
+    private final ChessClient chessClient;
     public State state = State.PreloginClient;
 
     public Repl(String serverUrl) {
-        preClient = new PreloginClient(serverUrl, this);
-        postClient = new PostloginClient(serverUrl, this);
-        gameClient = new GamePlayClient(serverUrl, this);
+        chessClient = new ChessClient(serverUrl, this);
     }
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
         var result = "";
-        System.out.print("\n" + preClient.eval("help") + "\n");
+        System.out.print("\n" + chessClient.eval("help") + "\n");
         while (!result.equals("quit")) {
             printPrompt();
             String line = scanner.nextLine();
             try {
-                result = switch (state) {
-                    case PreloginClient -> preClient.eval(line);
-                    case PostloginClient -> postClient.eval(line);
-                    case GamePlayClient -> gameClient.eval(line);
-                };
+                chessClient.eval(line);
                 System.out.println("\n" + result);
             } catch (Throwable e) {
                 var msg = e.toString();
