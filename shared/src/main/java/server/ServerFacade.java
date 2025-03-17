b17433c2cedf62653f8868b46a82f1a792960fbd
camchamb <cameronchamberlain1@gmail.com>
2025-03-17 11:57:51 -0600

    Added login to ServerFacade

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -2,6 +2,7 @@ package server;
 
 import com.google.gson.Gson;
 import model.*;
+import service.requests.*;
 
 import java.io.*;
 import java.net.*;
@@ -19,9 +20,12 @@ public class ServerFacade {
         return makeRequest("POST", path, u, UserData.class);
     }
 
+    public LoginResult login(UserData u) {
+        var path = "/session";
+        return makeRequest("POST", path, u, LoginResult.class);
+    }
 
-
-    public <T> T makeRequest(String method, String path,Object request, Class<T> objectClass) {
+    public <T> T makeRequest(String method, String path, Object request, Class<T> objectClass) {
         try {
             URL url = (new URI(serverUrl + path)).toURL();
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
