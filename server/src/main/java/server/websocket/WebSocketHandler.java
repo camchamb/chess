77ec4412dfa582d8a8505594ad8825c3251fb092
camchamb <cameronchamberlain1@gmail.com>
2025-03-28 14:52:51 -0600

    Added WebSocketHandler

---
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -0,0 +1,57 @@
+package server.websocket;
+
+
+import com.google.gson.Gson;
+import dataaccess.*;
+//import exception.ResponseException;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.Timer;
+
+
+@WebSocket
+public class WebSocketHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand action = new Gson().fromJson(message, UserGameCommand.class);
+//        switch (action.getCommandType()) {
+//            case CONNECT -> enter(action.visitorName(), session);
+//            case MAKE_MOVE -> exit(action.visitorName());
+//            case LEAVE -> enter(action.visitorName(), session);
+//            case RESIGN -> exit(action.visitorName());
+//            default -> ;
+//        }
+    }
+
+//    private void enter(String visitorName, Session session) throws IOException {
+//        connections.add(visitorName, session);
+//        var message = String.format("%s is in the shop", visitorName);
+//        var notification = new ServerMessage(ServerMessage.Type.ARRIVAL, message);
+//        connections.broadcast(visitorName, notification);
+//    }
+//
+//    private void exit(String visitorName) throws IOException {
+//        connections.remove(visitorName);
+//        var message = String.format("%s left the shop", visitorName);
+//        var notification = new ServerMessage(ServerMessage.Type.DEPARTURE, message);
+//        connections.broadcast(visitorName, notification);
+//    }
+//
+//    public void makeNoise(String petName, String sound) throws ResponseException {
+//        try {
+//            var message = String.format("%s says %s", petName, sound);
+//            var notification = new ServerMessage(ServerMessage.Type.NOISE, message);
+//            connections.broadcast("", notification);
+//        } catch (Exception ex) {
+//            throw new ResponseException(500, ex.getMessage());
+//        }
+//    }
+}
