004db7c437872f302338b1029d1db29c593c2761
camchamb <cameronchamberlain1@gmail.com>
2025-03-30 11:23:47 -0600

    Added AccessDAO to WebSocketHandler so that notifications will include correct name

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,46 +1,107 @@
 package server.websocket;
 
 
+import chess.ChessGame;
 import com.google.gson.Gson;
 import dataaccess.*;
 //import exception.ResponseException;
+import model.AuthData;
+import model.GameData;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import service.GameService;
 import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessages;
+import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
 
 import java.io.IOException;
-import java.util.Timer;
 
 
 @WebSocket
 public class WebSocketHandler {
 
     private final ConnectionManager connections = new ConnectionManager();
+    private final UserDAO userAccess;
+    private final GameDAO gameAccess;
+    private final AuthDAO authAccess;
+
+
+    public WebSocketHandler(UserDAO userAccess, GameDAO gameAccess, AuthDAO authAccess) {
+        this.userAccess = userAccess;
+        this.gameAccess = gameAccess;
+        this.authAccess = authAccess;
+    }
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws IOException {
+        System.out.println(message);
         UserGameCommand action = new Gson().fromJson(message, UserGameCommand.class);
         switch (action.getCommandType()) {
             case CONNECT -> connect(action.getAuthToken(), action.getGameID(), session);
 //            case MAKE_MOVE -> make_move(action.visitorName());
-//            case LEAVE -> leave(action.visitorName(), session);
+            case LEAVE -> leave(action.getAuthToken(), action.getGameID());
 //            case RESIGN -> resign(action.visitorName());
         }
     }
 
     private void connect(String authToken, int gameID, Session session) throws IOException {
         connections.add(authToken, gameID, session);
-        var message = String.format("%s is in the shop", authToken);
-        var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
+        String message;
+        GameData gameData;
+        try {
+            var authData = authAccess.getAuth(authToken);
+            if (authData == null) {
+                var error = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Bad Request");
+                connections.messageRoot(authToken, error);
+                return;
+            }
+            message = String.format("%s connected to the game", authData.username());
+            gameData = gameAccess.getGame(gameID);
+            if (gameData == null) {
+                var error = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "No Such Game");
+                connections.messageRoot(authToken, error);
+                return;
+            }
+        } catch (DataAccessException e) {
+            var error = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Bad Request");
+            connections.messageRoot(authToken, error);
+            return;
+        }
+        var loadGame = new LoadGameMessages(ServerMessage.ServerMessageType.LOAD_GAME, gameData.game());
+        connections.messageRoot(authToken, loadGame);
+        var notification = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
         connections.broadcast(authToken, gameID, notification);
     }
 
     private void leave(String authToken, int gameID) throws IOException {
         connections.remove(authToken);
-        var message = String.format("%s left the shop", authToken);
-        var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
+        String message;
+        GameData gameData;
+        AuthData authData;
+        try {
+            authData = authAccess.getAuth(authToken);
+            message = String.format("%s left the game", authData.username());
+            gameData = gameAccess.getGame(gameID);
+            if (gameData == null) {
+                var error = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "No Such Game");
+                connections.messageRoot(authToken, error);
+                return;
+            }
+            if (gameData.whiteUsername() != null && gameData.whiteUsername().equals(authData.username())) {
+                gameAccess.updateGame(new GameData(gameID, null, gameData.blackUsername(), gameData.gameName(), gameData.game()));
+            }
+            if (gameData.blackUsername() != null &&gameData.blackUsername().equals(authData.username())) {
+                gameAccess.updateGame(new GameData(gameID, gameData.whiteUsername(), null, gameData.gameName(), gameData.game()));
+            }
+        } catch (DataAccessException e) {
+            var error = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Bad Request");
+            connections.messageRoot(authToken, error);
+            return;
+        }
+        var notification = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
         connections.broadcast(authToken, gameID, notification);
     }
 //
